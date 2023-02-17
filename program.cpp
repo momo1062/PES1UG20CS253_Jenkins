c187f9d89b6fd88a66c17c136f024d9876b90a48
MIHIR SANTOSH SANJAY <78130783+momo1062@users.noreply.github.com>
2023-02-17 15:06:14 +0530

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,10 @@
+//File: file.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, This is Jenkins Pipeline\n";
+  cout << "PES1UG20CS253 has successfully built and run\n";
+  cout << "My name is Mihir Santosh Sanjay\n";
+  return 0;
+}
